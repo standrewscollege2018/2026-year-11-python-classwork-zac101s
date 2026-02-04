54261043f2c0ce6dd38b4f7b0250ca0c580a0b89
LinuxUser <63383816+zac101s@users.noreply.github.com>
2026-02-05 12:04:30 +1300

    This is the first

---
+++ first.py
@@ -0,0 +1,7 @@
+'''This program demonstatres print(), data types, variables, inputs and f-strings'''
+# print() is a function that outputs whatever is inside the brackets
+# numbers can be included directly in the brackets
+print(123)
+
+# when printing text, it must be in speecjmarks which turns it into a string
+print("Hello")
