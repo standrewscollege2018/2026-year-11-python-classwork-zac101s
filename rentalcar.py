fc81aef0f0a22056a14e52bed030f4a5dc5c0705
LinuxUser <63383816+zac101s@users.noreply.github.com>
2026-03-09 11:03:44 +1300

    Update rentalcar.py

--- rentalcar.py
+++ rentalcar.py
@@ -1,26 +1,50 @@
-'''Basic rental car system for a local universty'''
+'''Rental car system 2 based of the forst one.'''
 
+#Setting the lists for the cars, renter name, and if it is avalible
 cars = ["Suzuki Van (2)", "Toyota Corolla (4)", "Honda CRV (4)", "Suzuki Swift (4)", "Mitsubishi Airtrek (4)", "Nissian DC Ute (4)", "Toyota Previa (7) ", "Toyota Hi Ace 12", "Toyota Hi Ace (12)"]
-names = []
-bookloop = True 
+names = ["", "", "", "", "", "", "", "", "", ]
+available = [True, True, True, True, True, True, True, True, True, ]
 
+booking = True 
+selection = True
 
 print("Welcome to the Unversity vechiel rental system")
 print()
+print('If you would like to exit please enter 0')
 print("The vehicles are: ")
-
-
-for i in range(len(cars)):
-    print(f"{i+1} {cars[i]}")
-
 print()
 
-while bookloop == True:
+
+while selection == True:
+    for i in range(len(cars)):
+        if available[i] == True:
+            status = ""
+        else:
+            status = "- (Unavailable)"
+        print(f"{i+1} {cars[i]} {status}")
     try:
-        book = int(input("Which car would you like to book? ")) 
+        book = int(input("Please enter a car you would like to book "))
+        if book == 0:
+            print("Exiting booking system")
+            booking = False
+            selection = False
+            
+        elif book <= len(cars) and book >= 1:
+            if available[book -1] == False:
+                print("Sorry this car is already booked")
+            else:
+                name = input("What is your name you would book this car under? ")
+                if name.isspace():
+                    print("This only has spaces try again")
+                else:
+                    print(f"Thank you {name} you have booked {cars[book - 1]}")
+                    available.insert(book - 1, False)   
+                    names.insert(book - 1, name)
+        else:
+            print(f"Please enter a number from 1 to {len(cars)}")
+        
     except ValueError:
-        print("Please enter a vaild number")
-    if 0 <= len(cars):
-        print("It worked")
-    else:
-        print("It didnt work")
+        print("Please enter a valid number")
+
+         
+        
