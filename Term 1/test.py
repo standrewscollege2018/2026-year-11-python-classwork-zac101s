adb414b9d9909be555794526f9d7690189962472
LinuxUser <63383816+zac101s@users.noreply.github.com>
2026-04-30 11:58:58 +1200

    moving file

---
+++ Term 1/test.py
@@ -0,0 +1,15 @@
+'''This is a test'''
+
+first = True
+secound = False
+third = False
+
+while first == True:
+    print("Test1")
+
+
+while secound == True:
+    print("test2")
+
+while third == True:
+    print("Pumkin")
